820509c842e0fc298f4bd8e4974d1e88466e0ec1
zoe4561 <z0e.ts3ng@gmail.com>
2024-04-23 09:13:57 -0400

    third commit

---
+++ src/formatiftableaux.java
@@ -0,0 +1,11 @@
+public class formatiftableaux
+{
+    public static void main(String[] args)
+    {
+        int[] monTab = {5, 2, 23, 7, 87, 6, 5};
+        for (int i = 0; i <= monTab.length; i++)
+        {
+            System.out.println(monTab[i] + " ");
+        }
+    }
+}
---
+++ src/formatiftableaux1.java
@@ -0,0 +1,11 @@
+public class formatiftableaux1
+{
+    public  void main(String[] args)
+    {
+        saluer();
+    }
+     void saluer()
+    {
+        System.out.println("Bonjour");
+    }
+}
